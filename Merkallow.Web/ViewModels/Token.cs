f85bd82908264a0109a01b0445b29d7e22db9e25
Albert Garuda <algarud@gmail.com>
2022-03-21 23:40:18 +0200

    Token extensions & User models

--- Merkallow.Web/ViewModels/Token.cs
+++ Merkallow.Web/ViewModels/Token.cs
@@ -1,7 +1,38 @@
-﻿namespace Merkallow.Web.ViewModels
+﻿using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Merkallow.Web.ViewModels
 {
     public class Token
     {
         public string AccessToken { get; set; }
+    }
+
+    public class TokenPayload
+    {
+        public long Id { get; set; }
+        public string PublicAddress { get; set; }
+    }
+
+    public class TokenData
+    {
+        public TokenPayload Payload { get; set; }
+        public DateTime IssuedAt { get; set; }
+    }
+
+    public static class TokenExtensions
+    {
+        public static TokenData GetTokenFromJwt(this string cookie)
+        {
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(cookie);
+            var claim = jwt.Claims.First(o => o.Type == "payload").Value;
+            var payload = JsonConvert.DeserializeObject<TokenPayload>(claim);
+
+            return new TokenData()
+            {
+                IssuedAt = jwt.IssuedAt,
+                Payload = payload
+            };
+        }
     }
 }
